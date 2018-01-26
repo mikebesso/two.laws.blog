3165a714fad6705c02876d0b26bd9396bea60824
mikebesso <mike.besso@gmail.com>
2018-01-25 20:24:42 -0600

    setting default options

--- R/two.laws.blog.R
+++ R/two.laws.blog.R
@@ -1,18 +1,10 @@
-# Hello, world!
-#
-# This is an example function named 'hello' 
-# which prints 'Hello, world!'.
-#
-# You can learn more about package authoring with RStudio at:
-#
-#   http://r-pkgs.had.co.nz/
-#
-# Some useful keyboard shortcuts for package authoring:
-#
-#   Build and Reload Package:  'Cmd + Shift + B'
-#   Check Package:             'Cmd + Shift + E'
-#   Test Package:              'Cmd + Shift + T'
 
-hello <- function() {
-  print("Hello, world!")
+.onLoad <- function(libname, pkgname) {
+  options(
+    blogdown.ext = ".Rmd",
+    blogdown.subdir = "posts",
+    servr.daemon = TRUE,
+    blogdown.author = "Mike"
+  )
+
 }
